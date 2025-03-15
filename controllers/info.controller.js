02f0063ceb0fcf807a24fd9887aa59ca2c89da21
Tobias Hopp <tobias.hopp@alzura.com>
2025-03-15 05:21:23 +0100

    Endpoint info

--- controllers/info.controller.js
+++ controllers/info.controller.js
@@ -1,19 +1,19 @@
 const infoController = {
-	getInfo: async (req, res) => {
-		try {
-			const info = {
-				title: "Tobias Hopp - Personal API",
-				version: "0.0.1",
-				author: "Tobias Hopp",
-				license: "MIT",
-				website: "https://www.tobias-hopp.de",
-				contact: "mail@tobias-hopp.de"
-			};
-			res.status(200).json(info);
-		} catch (error) {
-			res.status(400).json({ error: error });
-		}
-	}
+  getInfo: async (req, res) => {
+    try {
+      const info = {
+        title: "WebDev HQ API",
+        version: "0.0.1",
+        author: "Tobias Hopp",
+        license: "MIT",
+        website: "https://webdev-hq.com",
+        contact: "admin@webdev-hq.com",
+      };
+      res.status(200).json(info);
+    } catch (error) {
+      res.status(400).json({ error: error });
+    }
+  },
 };
 
 module.exports = infoController;
