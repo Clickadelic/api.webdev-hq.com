68dc865e16a7fa0de275b81de6414dbb0e0d4419
clickadelic <toby.hopp@gmail.com>
2025-03-30 09:39:24 +0200

    Details

--- views/assets/js/copy-to-clipboard.js
+++ views/assets/js/copy-to-clipboard.js
@@ -3,7 +3,9 @@ if (window.location.pathname === "/") {
 	const apiUrl = document.querySelector("code")
 	copyToClipboard.addEventListener("click", () => {
 		apiUrl.setAttribute("style", "color: #1581e6")
+		apiUrl.innerText = "Copied!"
 		navigator.clipboard.writeText(apiUrl.innerText)
 		setTimeout(() => apiUrl.removeAttribute("style"), 2000)
+		setTimeout(() => (apiUrl.innerText = "https://api.webdev-hq.com/common/v1"), 2000)
 	})
 }
