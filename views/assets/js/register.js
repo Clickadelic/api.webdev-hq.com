6829b745749e86dabd0a5d8d3b7fbcbc3dd7dc81
clickadelic <toby.hopp@gmail.com>
2025-03-30 20:22:10 +0200

    Menu Button

--- views/assets/js/register.js
+++ views/assets/js/register.js
@@ -1,48 +1,31 @@
 if (window.location.pathname === "/register") {
-	document.addEventListener("DOMContentLoaded", function () {
+	document.addEventListener("DOMContentLoaded", () => {
 		const form = document.getElementsByTagName("form")[0]
 		const username = document.getElementById("username")
 		const email = document.getElementById("email") // Added email field
 		const password = document.getElementById("password")
 		const passwordRepeat = document.getElementById("password-repeat")
 		const agreedToTerms = document.getElementById("agreed-to-terms")
-		form.addEventListener("submit", async e => {
+
+		form.addEventListener("submit", e => {
 			e.preventDefault()
-
-			console.log(username.value, email.value, password.value, passwordRepeat.value, agreedToTerms.checked)
-
-			// Call the registerUser function here
-			await registerUser(username.value, email.value, password.value, passwordRepeat.value, agreedToTerms.checked)
+			console.log("Reading values")
+			if (!username.value || username.value.length < 4) {
+				return alert("min_3_characters")
+			}
+			if (!email.value || email.value.length < 4) {
+				return alert("min_3_characters")
+			}
+			if (!password.value || password.value.length < 6) {
+				return alert("min_6_characters")
+			}
+			if (!passwordRepeat.value || password.value != passwordRepeat.value) {
+				return alert("both_passwords_must_match")
+			}
+			if (!agreedToTerms.checked) {
+				return alert("terms_not_accepted")
+			}
+			form.submit()
 		})
 	})
-}
-
-const registerUser = async (username, email, password, passwordRepeat, agreedToTerms) => {
-	try {
-		const response = await fetch("/common/v1/auth/register", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				username: username,
-				email: email,
-				password: password,
-				passwordRepeat: passwordRepeat,
-				agreedToTerms: agreedToTerms
-			})
-		})
-
-		const result = await response.json()
-
-		if (result.error) {
-			return alert(result.error)
-		}
-
-		// Handle successful registration (e.g., redirect or show message)
-		alert("Registration successful!")
-	} catch (error) {
-		console.error("Registration error:", error)
-		alert("An error occurred during registration.")
-	}
 }
