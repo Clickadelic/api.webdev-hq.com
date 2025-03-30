9fd70af08393c90220d5be8e1af12da94efa5c3e
clickadelic <toby.hopp@gmail.com>
2025-03-30 21:30:32 +0200

    prisma

--- views/assets/js/register.js
+++ views/assets/js/register.js
@@ -35,7 +35,7 @@ const handleRegister = async e => {
 	console.log({ formData })
 
 	try {
-		await fetch("https://api.webdev-hq.com/common/v1/auth/register", {
+		await fetch("/common/v1/auth/register", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json"
