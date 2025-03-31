c8b454431e2c132925f2052e58574f7de7b2af31
clickadelic <toby.hopp@gmail.com>
2025-03-31 03:33:35 +0200

    Mega Menu

--- views/assets/js/mega-menu.js
+++ views/assets/js/mega-menu.js
@@ -2,8 +2,9 @@ console.log("Mega menu...")
 
 document.addEventListener("DOMContentLoaded", function () {
 	const menuBtn = document.getElementById("menu-btn")
+	const megaMenu = document.getElementsByClassName("mega-menu")[0]
 
 	menuBtn.addEventListener("click", () => {
-		alert("Menu")
+		megaMenu.classList.toggle("hidden")
 	})
 })
