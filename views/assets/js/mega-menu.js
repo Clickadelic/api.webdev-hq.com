43db263fe6967ff6358a64ef585b31949569e8c0
clickadelic <toby.hopp@gmail.com>
2025-03-31 05:22:42 +0200

    Mega Menu outside click close

--- views/assets/js/mega-menu.js
+++ views/assets/js/mega-menu.js
@@ -1,10 +1,17 @@
-console.log("Mega menu...")
-
 document.addEventListener("DOMContentLoaded", function () {
 	const menuBtn = document.getElementById("menu-btn")
 	const megaMenu = document.getElementsByClassName("mega-menu")[0]
 
-	menuBtn.addEventListener("click", () => {
+	menuBtn.addEventListener("click", event => {
+		event.stopPropagation()
+		menuBtn.classList.toggle("text-mantis-primary")
 		megaMenu.classList.toggle("hidden")
+	})
+
+	document.addEventListener("click", event => {
+		if (!menuBtn.contains(event.target) && !megaMenu.contains(event.target)) {
+			megaMenu.classList.add("hidden")
+			menuBtn.classList.remove("text-mantis-primary")
+		}
 	})
 })
