be5f74be28c49b9a0d67f021b34cf2c4c1e1f4ec
Clickadelic <toby.hopp@gmail.com>
2025-05-15 03:53:33 +0200

    Random image

--- controllers/extension.controller.js
+++ controllers/extension.controller.js
@@ -1,15 +1,43 @@
-require("dotenv").config()
+const createApi = require("unsplash-js").createApi
 
-import { createApi } from "unsplash-js"
-
-export const unsplash = createApi({
+const unsplash = createApi({
 	accessKey: process.env.UNSPLASH_ACCESS_KEY,
 	fetch: fetch
 })
 
 const extensionController = {
 	getDailyImage: async (req, res) => {
-		res.status(200).send({ message: "extension_image" })
+		try {
+			const collection = unsplash.photos
+				.getRandom({
+					query: "nature",
+					orientation: "landscape"
+				})
+				.then(response => {
+					res.status(200).json(response)
+				})
+				.catch(error => {
+					res.status(500).json({ error: error.message })
+				})
+			console.log(collection)
+		} catch (error) {
+			res.status(500).json({ error: error.message })
+		}
+	},
+	getRandomImage: async (req, res) => {
+		try {
+			unsplash.photos
+				.getRandom({
+					query: "nature",
+					orientation: "landscape"
+				})
+				.then(response => {
+					res.status(200).json(response)
+				})
+				.catch(error => {
+					res.status(500).json({ error: error.message })
+				})
+		} catch (error) {}
 	}
 }
 
