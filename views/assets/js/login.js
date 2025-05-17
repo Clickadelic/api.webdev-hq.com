cd8bfe3d6576d46c1a9ccd71e9f25d925b24df48
Clickadelic <toby.hopp@gmail.com>
2025-05-17 05:17:49 +0200

    Style

--- views/assets/js/login.js
+++ views/assets/js/login.js
@@ -22,7 +22,7 @@ const handleLogin = async e => {
 	const formData = { email, password }
 
 	try {
-		const response = await fetch("/common/v1/auth/login", {
+		const response = await fetch(`/common/v1/auth/login`, {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify(formData)
