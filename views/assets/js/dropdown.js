dec0e988012b5bbd5a266625f7a4d4d846ed20f0
Clickadelic <toby.hopp@gmail.com>
2025-05-22 03:28:56 +0200

    generic dropdowns

--- views/assets/js/dropdown.js
+++ views/assets/js/dropdown.js
@@ -1,27 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const button = document.getElementById("menu-button")
-	const accountMenu = document.getElementById("account-dropdown-menu")
+	const btns = document.getElementsByClassName("dropdown-btn")
+	const dropdowns = document.getElementsByClassName("dropdown-menu")
 
-	button.addEventListener("click", e => {
-		e.stopPropagation() // Verhindert sofortiges Schließen
-		accountMenu.classList.toggle("hidden")
-		const expanded = button.getAttribute("aria-expanded") === "true"
-		button.setAttribute("aria-expanded", String(!expanded))
-	})
+	for (let i = 0; i < btns.length; i++) {
+		btns[i].addEventListener("click", e => {
+			e.stopPropagation() // Verhindert sofortiges Schließen
+			dropdowns[i].classList.toggle("hidden")
+			const expanded = btns[i].getAttribute("aria-expanded") === "true"
+			btns[i].setAttribute("aria-expanded", String(!expanded))
+		})
+	}
 
-	// Klick außerhalb schließt Dropdown
-	document.addEventListener("click", e => {
-		if (!button.contains(e.target)) {
-			accountMenu.classList.add("hidden")
-			button.setAttribute("aria-expanded", "false")
-		}
-	})
-
-	// ESC-Taste schließt Dropdown
-	document.addEventListener("keydown", e => {
-		if (e.key === "Escape") {
-			accountMenu.classList.add("hidden")
-			button.setAttribute("aria-expanded", "false")
+	// Click outside handler
+	document.addEventListener("click", () => {
+		for (let i = 0; i < dropdowns.length; i++) {
+			if (!dropdowns[i].classList.contains("hidden")) {
+				dropdowns[i].classList.add("hidden")
+				btns[i].setAttribute("aria-expanded", "false")
+			}
 		}
 	})
 })
