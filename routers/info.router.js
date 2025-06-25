9ff663adc4e13a96097dbd41caf126176808173b
Clickadelic <toby.hopp@gmail.com>
2025-06-25 06:19:28 +0200

    Remove unused middleware import from info router

--- routers/info.router.js
+++ routers/info.router.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const infoRouter = express.Router()
-const middleware = require("../middleware")
 const infoController = require("../controllers/info.controller")
 
 infoRouter.get("/", infoController.getInfo)
