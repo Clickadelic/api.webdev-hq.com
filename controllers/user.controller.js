36f7bd2a74ba48c274c2a5363ea6d4e5d72b4cc6
Clickadelic <toby.hopp@gmail.com>
2025-07-29 00:46:16 +0200

    Copy/Paste Tailwind Dashboard

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -35,6 +35,21 @@ const userController = {
 		} catch (error) {
 			res.status(400).json({ message: error })
 		}
+	},
+	deleteUserById: async (req, res) => {
+		try {
+			const user = await prisma.user.delete({
+				where: {
+					id: req.params.id
+				}
+			})
+			if (user) {
+				user.password = undefined
+				res.status(200).json(user)
+			}
+		} catch (error) {
+			res.status(400).json({ message: error })
+		}
 	}
 }
 
