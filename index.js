8bc679e6b33d03bb5714727023a4479ae621a68d
Clickadelic <toby.hopp@gmail.com>
2025-07-31 12:00:24 +0200

    log

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const chalk = require("chalk")
 const PORT = process.env.PORT || 3000
 
 const server = app.listen(PORT, () => {
-	console.log(chalk.greenBright("----------------------------------------"))
-	console.log(chalk.blueBright("----- Welcome to WebDev HQ Web API -----"))
-	console.log(chalk.greenBright("----------------------------------------"))
-	console.log(chalk.blueBright(">>>>>>>> " + `${process.env.APP_URL}:${PORT}`, " <<<<<<<<"))
-	console.log(chalk.greenBright("----------------------------------------"))
+	console.log(chalk.whiteBright("----------------------------------------"))
+	console.log(chalk.greenBright(">> WebDev HQ Web API - up and running <<"))
+	console.log(chalk.whiteBright("----------------------------------------"))
+	console.log(chalk.whiteBright(">>>>>>> @" + `${process.env.APP_URL}:${PORT}`, " <<<<<<<<"))
+	console.log(chalk.whiteBright("----------------------------------------"))
 })
 
 const shutdown = async (signal) => {
