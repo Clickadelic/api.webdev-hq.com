ebf5912525d4c95e4e92bce462f25b2cf4500a73
Clickadelic <toby.hopp@gmail.com>
2025-10-10 03:09:20 +0200

    myController

--- routers/my.router.js
+++ routers/my.router.js
@@ -3,9 +3,9 @@ const myRouter = express.Router()
 const myController = require("../controllers/my.controller")
 const middleware = require("../middleware")
 myRouter.get("/my/links", middleware.verifyTokenFromCookie, myController.getMyLinks)
-myRouter.get("/my/links/:id", middleware.verifyTokenFromCookie,linkController.getLinkById)
-myRouter.post("/my/links", middleware.verifyTokenFromCookie, linkController.createLink)
-myRouter.patch("/my/links/:id", middleware.verifyTokenFromCookie, linkController.patchLinkById)
-myRouter.delete("/my/links/:id", middleware.verifyTokenFromCookie, linkController.deleteLinkById)
+myRouter.get("/my/links/:id", middleware.verifyTokenFromCookie,myController.getLinkById)
+myRouter.post("/my/links", middleware.verifyTokenFromCookie, myController.createLink)
+myRouter.patch("/my/links/:id", middleware.verifyTokenFromCookie, myController.patchLinkById)
+myRouter.delete("/my/links/:id", middleware.verifyTokenFromCookie, myController.deleteLinkById)
 
 module.exports = myRouter
