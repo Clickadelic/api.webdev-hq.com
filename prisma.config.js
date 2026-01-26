325ba0224f68b5115d5d54d74b05212d7caf61b1
Clickadelic <toby.hopp@gmail.com>
2026-01-26 03:54:08 +0100

    db url test

--- prisma.config.js
+++ prisma.config.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
+const databaseUrl = process.env.DATABASE_URL
 // Test
 export default defineConfig({
   schema: "prisma/schema.prisma",
