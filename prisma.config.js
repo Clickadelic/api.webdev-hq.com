4b74aa58ae1280c244c949d67679a7cd300f05df
Clickadelic <toby.hopp@gmail.com>
2026-01-26 03:50:37 +0100

    db url

--- prisma.config.js
+++ prisma.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
     provider: "mysql",
-    url: process.env.DATABASE_URL,
+    url: env(DATABASE_URL),
   },
   migrations: {
     path: "prisma/migrations",
