7aaba1e4954350e07a4b1f904a5997e8212bd7c8
Clickadelic <toby.hopp@gmail.com>
2026-01-26 03:24:09 +0100

    fix test

--- prisma.config.js
+++ prisma.config.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
+const DATABASE_URL = process.env.DATABASE_URL;
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
     provider: "mysql",
-    url: env(DATABASE_URL),
+    url: DATABASE_URL,
   },
   migrations: {
     path: "prisma/migrations",
