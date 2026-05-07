bf9546a480b55ee77c15e92fde4fc4adf60f8a5b
Clickadelic <toby.hopp@gmail.com>
2026-05-07 17:09:41 +0200

    Fix e-mail and registration mail

--- mail/index.js
+++ mail/index.js
@@ -1,16 +1,14 @@
 const nodemailer = require("nodemailer")
 
-// TODO: Increase transport security by using environment variables for sensitive data
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: 587,
-  secure: false, // STARTTLS
-  requireTLS: true,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  }
-});
-
+	host: process.env.MAIL_HOST,
+	port: process.env.MAIL_PORT,
+	secure: false, // STARTTLS
+	requireTLS: true,
+	auth: {
+		user: process.env.MAIL_USER,
+		pass: process.env.MAIL_PASSWORD
+	}
+})
 
 module.exports = transporter
