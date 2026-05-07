19b53b9a92f43ee67a1c94de45d08af9f70e0315
Clickadelic <toby.hopp@gmail.com>
2026-05-08 00:23:55 +0200

    Chrome endpoints removed from docs

--- swagger/specs.js
+++ swagger/specs.js
@@ -4,13 +4,11 @@ const swaggerOptions = {
 	definition: {
 		openapi: "3.0.0",
 		info: {
-			title: "WebDev HQ Api-Documentation",
+			title: "WebDev HQ - Api-Documentation",
 			version: "0.1.6",
-			description: "API Endpoints for https://api.webdev-hq.com",
+			description: "API Endpoints for https://api.webdev-hq.com"
 		},
-		servers: [
-			{ url: process.env.API_URL || "http://localhost:5000" },
-		],
+		servers: [{ url: process.env.API_URL || "http://localhost:5000" }],
 		components: {
 			schemas: {
 				User: {
@@ -18,16 +16,16 @@ const swaggerOptions = {
 					properties: {
 						id: { type: "string", example: "43132374-5ac1-4db7-8c93-65ef4a8c6573" },
 						username: { type: "string", example: "JohnDev" },
-						email: { type: "string", example: "john@dev.com" },
-					},
+						email: { type: "string", example: "john@dev.com" }
+					}
 				},
 				Post: {
 					type: "object",
 					properties: {
 						id: { type: "integer", example: 1 },
 						title: { type: "string", example: "My first post" },
-						content: { type: "string", example: "with some content..." },
-					},
+						content: { type: "string", example: "with some content..." }
+					}
 				},
 				Links: {
 					type: "object",
@@ -36,13 +34,13 @@ const swaggerOptions = {
 						title: { type: "string", example: "Best Library" },
 						description: { type: "string", example: "Description with some content..." },
 						url: { type: "string", example: "https://mybestlibrary.com" },
-						isPublic: { type: "boolean", example: "https://mybestlibrary.com" },
-					},
-				},
-			},
-		},
+						isPublic: { type: "boolean", example: "https://mybestlibrary.com" }
+					}
+				}
+			}
+		}
 	},
-	apis: ["./routers/*.js"], 
+	apis: ["./routers/*.js"]
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
