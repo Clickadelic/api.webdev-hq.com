d6da488a37623e408489e9cd7dde2abc92cdc617
Clickadelic <toby.hopp@gmail.com>
2026-05-07 17:24:46 +0200

    Template fixes env

--- mail/index.js
+++ mail/index.js
@@ -1,14 +1,13 @@
 const nodemailer = require("nodemailer")
 
-// TODO: Increase transport security by using environment variables for sensitive data
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: 587,
+  port: process.env.MAIL_PORT ? parseInt(process.env.MAIL_PORT, 10) : 587,
   secure: false, // STARTTLS
   requireTLS: true,
   auth: {
     user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
+    pass: process.env.MAIL_PASSWORD
   }
 });
 
